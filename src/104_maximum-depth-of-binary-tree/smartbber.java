1cceb7dcc81940023cbf58fce6f46d8d8a698c4f
smartBBer <bai5775@outlook.com>
2018-12-30 15:47:15 +0800

    by smartbber

---
+++ src/104_maximum-depth-of-binary-tree/smartbber.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            int left_height = maxDepth(root.left);
+            int right_height = maxDepth(root.right);
+            return Math.max(left_height, right_height) + 1;
+        }
+    }
+}
