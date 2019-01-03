65baeaba1ccf63abe9f8f9a9b880b7b8ab970d19
smartBBer <bai5775@outlook.com>
2019-01-03 14:46:03 +0800

    by smartbber

---
+++ src/167_two-sum-ii-input-array-is-sorted/smartbber.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        boolean isTrue = false;
+        for(int i=0; i < numbers.length; i++) {
+            int tmp = target - numbers[i];
+            ans[0] = i + 1;
+            for(int j = i + 1; j < numbers.length; j++) {
+                if(tmp - numbers[j] == 0) {
+                    ans[1] = j + 1;
+                    isTrue = true;
+                    break;
+                }
+            }
+            if(isTrue) {
+                break;
+            }
+        }
+        return ans;
+    }
+}
