699dccb234865f59a8d3dd7e42a5c3842c9cc843
smartBBer <bai5775@outlook.com>
2019-01-04 10:27:20 +0800

    by smartbber

---
+++ src/169_/smartbber.java
@@ -0,0 +1,20 @@
+//摩尔投票法
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1, maj = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if(maj == nums[i]) { 
+                count++;
+            } else {
+                count--;
+                if(count == 0) {
+                    maj = nums[i];
+                    count = 1;
+                }
+            }
+            
+        }
+        return maj;
+    }
+}
