c1543b8bb57c7f92d2a0945373610b1a415c562f
smartBBer <bai5775@outlook.com>
2019-01-05 21:28:28 +0800

    by smartbber

---
+++ src/231_power-of-two/smartbber.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0)
+            return false;
+        if((n & (n - 1)) == 0)
+            return true;
+        return false;
+    }
+}
