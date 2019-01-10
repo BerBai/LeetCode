b75893857a35541a3dff30d1fc569f87373a8e99
smartBBer <bai5775@outlook.com>
2019-01-10 23:01:13 +0800

    by smartbber

--- src/122_best-time-to-buy-and-sell-stock-ii/smartbber.java
+++ src/122_best-time-to-buy-and-sell-stock-ii/smartbber.java
@@ -50,7 +50,7 @@ class Solution {
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] > prices[i - 1])
                 maxprofit += prices[i] - prices[i - 1];
-        } 
+        }
         return maxprofit;
     }
 }
