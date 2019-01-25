a8fb0adc9bd71e0eccbd1b32a887173b3d0f951e
smartBBer <bai5775@outlook.com>
2019-01-25 18:48:20 +0800

    by samrtbber

---
+++ src/203_/smartbber.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    //暴力
+    public int rangeBitwiseAnd(int m, int n) {
+        int ans = n;
+        for(int i = m; i < n; i++) {
+            ans = ans & i;
+        }
+        
+        return ans;
+    }
+    
+    //
+    public int rangeBitwiseAnd1(int m, int n) {
+        int ans = m & n;
+        int len = n - m + 1;
+        int i = 1;
+        if(len > 2) {
+            while(true) {
+                if(i >= len - 1) {
+                    ans = ans & (Integer.MAX_VALUE - i);
+                    break;
+                }
+                i = (i << 1) + 1;
+            }
+        }
+        //System.out.println(Integer.MAX_VALUE);
+        return ans;
+    }
+}
