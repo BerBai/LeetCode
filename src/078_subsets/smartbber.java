44fdbaef9c4a3471927092f18eb3b338d3690bf3
smartBBer <bai5775@outlook.com>
2019-02-13 16:25:15 +0800

    by smartbber

--- src/078_subsets/smartbber.java
+++ src/078_subsets/smartbber.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>());
-        for(int n : nums){
-            int size = result.size();
-            for(int i=0; i<size; i++){
-                List<Integer> subset = new ArrayList<>(result.get(i));
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        for(int n : nums) { //获取数组的每一个值
+            int size = ans.size();
+            for(int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(ans.get(i));
                 subset.add(n);
-                result.add(subset);
+                ans.add(subset);
             }
         }
-        return result;
+        return ans;
     }
 }
