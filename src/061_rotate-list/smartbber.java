c043ddfc246f7680230a91470b4904b8812b3ea6
smartBBer <bai5775@outlook.com>
2019-03-20 23:15:21 +0800

    fix

--- src/061_rotate-list/smartbber.java
+++ src/061_rotate-list/smartbber.java
@@ -11,21 +11,22 @@ class Solution {
         if(head == null || head.next == null || k == 0)
             return head;
         int len = 1;
-        ListNode p1 = head, end = null;
+        ListNode p1 = head, end = null;//end标记尾节点
+        //计算出链表长度
         while(p1.next != null) {
             len++;
             p1 = p1.next;
         }
-        int move = len - k % len;
-        end = p1;
-        p1.next = head;
-        p1 = head;
+        int move = len - k % len; //计算要向左循环的次数，题目为向右
+        end = p1; //p1为尾结点
+        p1.next = head; //修改为循环链表
+        p1 = head; //p1指向头结点
         while(move!=0) {
             p1 = p1.next;
             end = end.next;
             move--;
         }
-       end.next = null;
+       end.next = null; //断开链表
         return p1;
     }
 }
