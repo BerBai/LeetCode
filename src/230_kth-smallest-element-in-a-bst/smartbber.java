6bf7c0dcf69d0efb8838774587f4d35e4a238d38
smartBBer <bai5775@outlook.com>
2019-03-21 23:19:34 +0800

    fix

--- src/230_kth-smallest-element-in-a-bst/smartbber.java
+++ src/230_kth-smallest-element-in-a-bst/smartbber.java
@@ -8,9 +8,10 @@
  * }
  */
 class Solution {
+    //将二叉树写入list
     List<Integer> list = new ArrayList<>();
     int num = 0;
-    public int kthSmallest(TreeNode root, int k) {
+    public int kthSmallestList(TreeNode root, int k) {
         num = k;
         sovle(root);
         return list.get(k-1);
@@ -23,5 +24,23 @@ class Solution {
             return;
         list.add(root.val);
         sovle(root.right);
+    }
+    
+    //中序遍历
+    int time = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        int ans = -1;
+        if(root == null) {
+            return ans;
+        }
+        
+        ans = kthSmallest(root.left, k);
+        if(time == k) {
+            return ans;
+        }
+        if(++time == k) {
+            return root.val;
+        }
+        return kthSmallest(root.right, k);
     }
 }
