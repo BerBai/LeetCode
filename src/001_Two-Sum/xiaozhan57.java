f573e16d47624d1289df7743919d590ec0411fd8
柏建雄 <30593903+xiaozhan57@users.noreply.github.com>
2019-03-30 23:39:11 +0800

    Create xiaozhan57.java

---
+++ src/001_Two-Sum/xiaozhan57.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            int tmp = target - nums[i];
+            for(int j = i+1; j < nums.length; j++) {
+                if(tmp == nums[j] && i != j)
+                    return new int[]{i,j};
+            }
+        }
+        throw new IllegalArgumentException("");
+    }
+}
