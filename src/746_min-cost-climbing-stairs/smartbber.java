47312890b6ae37aa7a07aad430da67277278f393
smartBBer <bai5775@outlook.com>
2019-04-01 21:55:16 +0800

    update

---
+++ src/746_min-cost-climbing-stairs/smartbber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        int dp[] = new int[len + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < len; i++){
+            dp[i] = 0;
+        }
+        for(int i = 2; i < len; i++){
+            dp[i] = Math.min(dp[i-1],dp[i-2]) + cost[i];
+        }
+        return Math.min(dp[len-1],dp[len-2]);
+    }
+}
