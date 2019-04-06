bf301f9a2a04a6bb583914e6e8deaf755cbce494
smartBBer <bai5775@outlook.com>
2019-04-06 21:15:31 +0800

    update

--- src/486_predict-the-winner/smartbber.java
+++ src/486_predict-the-winner/smartbber.java
@@ -1,16 +1,15 @@
 class Solution {
     //如果是偶数个数，先手总能拿到奇数列或偶数列数，即总能拿到最大数
     //否则，进行判断
+    //
     public boolean PredictTheWinner(int[] nums) {
         int len = nums.length;
         int[][] dp = new int[len+1][len+1];
-        int sum = 0;
         if(len % 2 == 0 || len <= 1)
             return true;
         else {
             for(int i = 0;i<len;i++) {
                 int total = 0;
-                sum += nums[i];
                 for(int j=i;j<len;j++){
                     total += nums[j];
                     dp[j][i] = total;
@@ -21,7 +20,7 @@ class Solution {
                     dp[i][j] = dp[j][i] - Math.min(dp[i][j - 1], dp[i + 1][j]);
                 }
             }
-            if(dp[0][len-1]*2>=sum)
+            if(dp[0][len-1]*2>=dp[len-1][0])
                 return true;
         }
         return false;
