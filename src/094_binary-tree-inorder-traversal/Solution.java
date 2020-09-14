48ee34e61a0a8802c779f59e8094af08bf68bad3
tooyi <bai5775@outlook.com>
2020-09-14 15:00:03 +0800

    94

---
+++ src/094_binary-tree-inorder-traversal/Solution.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution{
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+
+}
