822b0f68a2008f4e107b17108fceb18dfe127e27
bai5775 <bai5775@outlook.com>
2020-09-16 09:22:36 +0800

    226

---
+++ src/226_invert-binary-tree/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
